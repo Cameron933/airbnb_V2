bf0e0454cf9f8db62fc04f4cba2e01565ac0ca81
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-03-06 01:44:33 +1100

    feat: finish the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 gap-y-10 bg-gray-100 px-32 py-14 text-gray-600 md:grid-cols-4">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is Not a Real Site</p>
+        <p>Its a Pretty Awesome Clone</p>
+        <p>Referrals Accepted</p>
+        <p>Happy Coding</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Cam React</p>
+        <p>Presents</p>
+        <p>Full Stack Developer</p>
+        <p>Dozens of Projects</p>
+        <p>Join Now</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Banner from "@/components/Banner";
 import SmallCard from "@/components/SmallCard";
 import MediumCard from "@/components/MediumCard";
 import LargeCard from "@/components/LargeCard";
+import Footer from "@/components/Footer";
 
 export default function Home({ exploreData, cardsData }: any) {
   return (
@@ -50,6 +51,8 @@ export default function Home({ exploreData, cardsData }: any) {
           buttonText="Get Inspired"
         />
       </main>
+
+      <Footer />
     </div>
   );
 }
