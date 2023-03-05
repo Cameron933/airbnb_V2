38800fb780cdd41ee06683d42125d3c12911b768
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-03-05 20:05:27 +1100

    feat: finish the explore nearby section

---
+++ src/components/SmallCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+type Card = {
+  img: string;
+  location: string;
+  distance: string;
+};
+
+const SmallCard = ({ img, location, distance }: Card) => {
+  return (
+    <div className="m-2 mt-5 flex transform cursor-pointer items-center space-x-4 rounded-xl transition duration-200 ease-in-out hover:scale-105 hover:bg-gray-100">
+      <div className="relative h-16 w-16">
+        <Image src={img} alt="property photo" fill className="rounded-lg" />
+      </div>
+      <div>
+        <h2>{location}</h2>
+        <h3 className="text-gray-500">{distance}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default SmallCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import SmallCard from "@/components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }: any) {
   return (
     <div className="">
       <Head>
@@ -14,6 +15,28 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="mx-auto max-w-7xl px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
+          {/* pull some data from a server -API points */}
+          <div className="lg:grid-col-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item: { img: string; location: string; distance: string }) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then((res) => res.json());
+  return { props: { exploreData } };
 }
